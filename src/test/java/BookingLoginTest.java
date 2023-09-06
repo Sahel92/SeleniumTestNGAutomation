fc387f045b4dc5463c154b7ec8426a23e8a670af
Sahel92 <125098911+Sahel92@users.noreply.github.com>
2023-09-06 01:46:15 -0700

    Update BookingLoginTest.java
    Log4j reports are now formatted properly.

--- src/test/java/BookingLoginTest.java
+++ src/test/java/BookingLoginTest.java
@@ -1,3 +1,4 @@
+
 import com.sahel.booking.base.TestBase;
 
 import org.testng.Assert;
@@ -6,23 +7,30 @@ import org.testng.annotations.Test;
 
 public class BookingLoginTest extends TestBase {
 
-    // Performs login action.
-    // Login scenarios cannot be automated for Booking.com. Unfortunate.....
-    @Parameters({"email", "password"})
-    @Test
-    public void performSignIn(String email, String password) {
-        Assert.assertEquals(factory.homePage().getTitle(), "Booking.com | Official site | The best hotels, flights, car rentals" + " & " + "accommodations");
-        logger.info("User is on Booking.com homepage");
-        factory.homePage().clickSignIn();
-        logger.info("User clicked on 'SignIn' button");
-        factory.signInPage().enterEmail(email);
-        logger.info("User entered valid email");
-        factory.signInPage().clickContinueWithEmail();
-        logger.info("User clicked 'Continue with email' button");
-        factory.signInPage().enterPassword(password);
-        logger.info("User entered valid password");
-        factory.signInPage().clickSignIn();
-        logger.info("User clicked 'Sign in' button on the sign page");
+// Author - Sahel92
+
+    // As a fun little exercise you can go ahead and uncomment it and play around with the code.
+
+    
+    // Demo test scenario for automating the login process
+    // However captcha automation is not feasible with Selenium WebDriver
+    // However this serves as a good example of the steps a tester might take at "work" (or fun time as I call it)
+   
+//    @Parameters({"email", "password"})
+//    @Test
+//    public void performSignIn(String email, String password) {
+        // Assert.assertEquals(factory.homePage().getTitle(), "Booking.com | Official site | The best hotels, flights, car rentals" + " & " + "accommodations");
+        // logger.info("User is on Booking.com homepage");
+        // factory.homePage().clickSignIn();
+        // logger.info("User clicked on 'SignIn' button");
+        // factory.signInPage().enterEmail(email);
+        // logger.info("User entered valid email");
+        // factory.signInPage().clickContinueWithEmail();
+        // logger.info("User clicked 'Continue with email' button");
+        // factory.signInPage().enterPassword(password);
+        // logger.info("User entered valid password");
+        // factory.signInPage().clickSignIn();
+        // logger.info("User clicked 'Sign in' button on the sign page");
     }
 
 
